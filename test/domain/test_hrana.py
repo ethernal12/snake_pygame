2a2221971869c042f7e48384da1bc072a0ddc39d
DanijelK <korbar41@gmail.com>
2023-02-02 16:35:03 +0100

    Test naključnega re-postavitve hrane.

--- test/domain/test_hrana.py
+++ test/domain/test_hrana.py
@@ -1,6 +1,7 @@
 import unittest
 
 from src.domain.hrana import Hrana
+from src.domain.zemlja import Zemlja
 
 
 class Test_Hrana(unittest.TestCase):
@@ -9,12 +10,16 @@ class Test_Hrana(unittest.TestCase):
         self.hrana_x = 5
         self.hrana_y = 2
         self.hrana = Hrana(x=self.hrana_x, y=self.hrana_y)
+        self.zemlja = Zemlja(10, 10)
 
     def test___init__(self):
-        self.assertEqual(self.hrana.x, self.hrana_x)
+        self.assertTrue(self.hrana.x, self.hrana_x)
         self.assertEqual(self.hrana.y, self.hrana_y)
 
     def test_nakljucne_postavitve_hrane(self):
-
-
-
+        for i in range(10):
+            self.zemlja.nakljucna_vrednost()
+            x = self.zemlja.hrana.x
+            y = self.zemlja.hrana.y
+            self.assertTrue(1 <= x <= self.zemlja.sirina - 1)
+            self.assertTrue(1 <= y <= self.zemlja.sirina - 1)
